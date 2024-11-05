a1578df2834607fa28df9f134301f5a7518a3f15
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2024-11-05 09:15:53 -0300

    feat: signup endpoint skeleton

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 
 	route := http.NewServeMux()
 	route.HandleFunc("GET /hello", auth.WithJWT(handleHello))
+	route.HandleFunc("POST /signup", handleSignup)
 
 	v1 := http.StripPrefix("/api/v1", route)
 
@@ -22,4 +23,18 @@ func main() {
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	name := r.Context().Value(types.UserName)
 	httputils.WriteJSON(w, http.StatusOK, fmt.Sprintf("Hello, %s", name))
+}
+
+func handleSignup(w http.ResponseWriter, r *http.Request) {
+	// decode payload
+
+	// validate payload
+
+	// check if user already exists
+
+	// persist user in database
+
+	// create user in keycloak
+
+	// return response
 }
